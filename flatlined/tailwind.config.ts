ad1550e2c0f615d6e290e1bfa84450814c278b4a
Justin Chen <jchen4086@gmail.com>
2024-03-23 12:40:00 -0400

    shadcn init

--- flatlined/tailwind.config.ts
+++ flatlined/tailwind.config.ts
@@ -1,27 +1,51 @@
-import type { Config } from "tailwindcss";
-
+import type { Config } from "tailwindcss"
 const colors = require('tailwindcss/colors');
+const { fontFamily } = require("tailwindcss/defaultTheme")
 
-const config: Config = {
+const config = {
+  darkMode: ["class"],
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
   ],
+  prefix: "",
   theme: {
+    container: {
+      center: true,
+      padding: "2rem",
+      screens: {
+        "2xl": "1400px",
+      },
+    },
     extend: {
+      fontFamily: {
+        poppins: ['var(--font-poppins)', ...fontFamily.sans],
+      },
       backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
         'hero': "url('/hero.jpg')"
       },
       screens: {
         'xs': '540px'
       },
+      keyframes: {
+        "accordion-down": {
+          from: { height: "0" },
+          to: { height: "var(--radix-accordion-content-height)" },
+        },
+        "accordion-up": {
+          from: { height: "var(--radix-accordion-content-height)" },
+          to: { height: "0" },
+        },
+      },
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
+      },
       colors: {
         'null-1':          '#28262E',
-        'null-body':             '#171922',
+        'background':             '#171922',
         'null-3':             '#252531',
         'null-sidebar-head':  '#24242E',
         'null-sidebar-body':  '#20202A',
@@ -43,6 +67,7 @@ const config: Config = {
       }
     },
   },
-  plugins: [],
-};
-export default config;
+  plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
