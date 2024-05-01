c92786398ac83ecd3131278a0c6ac57d56f529ec
Justin Chen <jchen4086@gmail.com>
2024-05-01 11:49:24 -0400

    escaping char fix

--- flatlined/components/LandingLayout.tsx
+++ flatlined/components/LandingLayout.tsx
@@ -61,13 +61,13 @@ const LandingLayout: React.FC<LandingLayoutProps> = ({
           {`Hey, I'm Justin`}
         </div>
         <div className='text-offwhite py-4 leading-7 mr-8 xl:mr-12'>
-          I used to have bios pre-written for the purpose of autofilling queries like these. I stopped writing them because I realized that I outgrow them every couple of weeks. The self-described me in those descriptions no longer represent who I am today. Now I just write whatever comes off the top of my head.
+          {`I used to have bios pre-written for the purpose of autofilling queries like these. I stopped writing them because I realized that I outgrow them every couple of weeks. The self-described me in those descriptions no longer represent who I am today. Now I just write whatever comes off the top of my head.`}
           <br /> <br />
-          I am a jack of all trades. I can code in pretty much any language. I can make websites using simple web builders like Wix or Wordpress to more complicated frameworks like Next.js and Svelte. I can develop games using Godot, Pygame, and Unity. I can train an AI model using TensorFlow to detect when you are sitting with bad posture (looking at you).
+          {`I am a jack of all trades. I can code in pretty much any language. I can make websites using simple web builders like Wix or Wordpress to more complicated frameworks like Next.js and Svelte. I can develop games using Godot, Pygame, and Unity. I can train an AI model using TensorFlow to detect when you are sitting with bad posture (looking at you).`}
           <br /> <br />
-          I am also not limited to programming. I know how to use Affinity and Canva to manipulate designs and photos. I can write scores using Musescore and produce a song in FL Studio. I can edit and render videos and films using DaVinci Resolve. I can squat and dead lift 225lbs for 8 reps and run a mile afterwards.
+          {`I am also not limited to programming. I know how to use Affinity and Canva to manipulate designs and photos. I can write scores using Musescore and produce a song in FL Studio. I can edit and render videos and films using DaVinci Resolve. I can squat and dead lift 225lbs for 8 reps and run a mile afterwards.`}
           <br /> <br />
-          I can do all of these things. And that ever-growing list of things is the reason why bios like these get rewritten. And it's a good thing. Every new bio is symbolic of a new me that has gained something since the last one. It represents my dynamic growth and ability to adapt. And one day I want to have contributed something that truly <span className='text-cyberviolet'>mattered,</span>, for someone, to someone.
+          {`I can do all of these things. And that ever-growing list of things is the reason why bios like these get rewritten. And it's a good thing. Every new bio is symbolic of a new me that has gained something since the last one. It represents my dynamic growth and ability to adapt. And one day I want to have contributed something that truly `}<span className='text-cyberviolet'>{`mattered`}</span>{`, for someone, to someone.`}
         </div>
       </div>
       <div className='flex flex-wrap gap-6 max-w-[400px] py-3'>
