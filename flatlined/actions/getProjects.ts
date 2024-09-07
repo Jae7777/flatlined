377ff8ccefc29a62fe51e044db50794355edde2c
Justin Chen <jchen4086@gmail.com>
2024-09-06 20:22:48 -0400

    Update getProjects.ts

--- flatlined/actions/getProjects.ts
+++ flatlined/actions/getProjects.ts
@@ -2,7 +2,7 @@ import client from "@/app/lib/contentful";
 
 const getProjects = async() => {
   const response = await client.getEntries({
-    content_type: 'projectCard',
+    content_type: 'project',
   });
   
   return response.items;
