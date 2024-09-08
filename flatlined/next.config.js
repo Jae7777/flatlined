415e42748a19b36860918066f030d84bf984462f
Justin Chen <jchen4086@gmail.com>
2024-09-08 13:00:02 -0400

    env scope fix

--- flatlined/next.config.js
+++ flatlined/next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
       pathname: '**'
     }]
   },
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+  },
 }
 
 module.exports = nextConfig
