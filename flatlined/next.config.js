2582314a64c8423aa6579213ce696a01554aab7c
Justin Chen <jchen4086@gmail.com>
2024-09-08 13:03:05 -0400

    next configs

--- flatlined/next.config.js
+++ flatlined/next.config.js
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{
-      protocol: 'https',
-      hostname: "res.cloudinary.com",
-      pathname: '**'
-    }]
+    
   },
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
