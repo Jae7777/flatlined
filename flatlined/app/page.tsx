fcdc4415703a3f654f3ff9b73fbcad88d242febf
Justin Chen <jchen4086@gmail.com>
2024-09-11 14:42:09 -0400

    fix

--- flatlined/app/page.tsx
+++ flatlined/app/page.tsx
@@ -1,7 +1,6 @@
-import { LampContainer } from "@/components/ui/lamp";
-import { motion } from "framer-motion";
 import { fira_code } from "./fonts";
 import { Metadata } from "next";
+import Hero from "@/components/Hero";
 
 export const dynamic = 'force-dynamic';
  
@@ -39,21 +38,8 @@ export const metadata: Metadata = {
 
 export default function Home() {
   return (
-    <div className={`${fira_code.className}`}>
-      <LampContainer>
-        <motion.h1
-          initial={{ opacity: 0.5, y: 100 }}
-          whileInView={{ opacity: 1, y: 0 }}
-          transition={{
-            delay: 0.3,
-            duration: 0.8,
-            ease: "easeInOut",
-          }}
-          className="mt-8 bg-gradient-to-br from-slate-300 to-slate-500 py-4 bg-clip-text text-center text-4xl font-medium tracking-tight text-transparent md:text-7xl"
-        >
-          The visionary coding studio.
-        </motion.h1>
-      </LampContainer>
-    </div>
+    <main className={`${fira_code.className}`}>
+      <Hero />
+    </main>
   );
 }
